3388bfe860b3a993d19993c3eb6643c2f1c37c7f
MahdiGhafarian <mahdi.ghaf@email.com>
2025-02-25 13:40:20 -0600

    modified:   financial-markets-explorer/app.py

--- financial-markets-explorer/app.py
+++ financial-markets-explorer/app.py
@@ -316,6 +316,8 @@ st.subheader('Data')
 
 # Create Dataframe to display latest data
 df2 = df[['Open','High','Low','Close','avg','l_avg']]
+# Calculate change
+df2['Change'] = df2['Close'].diff()
 # Calculate percentage change
 df2['Change (%)'] = df2['Close'].pct_change() * 100
 # Change index display format
