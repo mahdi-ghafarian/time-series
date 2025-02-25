fd2d9d6f161d193b8286481e68cf2a7824b20f64
MahdiGhafarian <mahdi.ghaf@email.com>
2025-02-25 09:42:17 -0600

    modified:   financial-markets-explorer/app.py

--- financial-markets-explorer/app.py
+++ financial-markets-explorer/app.py
@@ -314,10 +314,8 @@ with st.expander('Latest Data'):
     # Create Dataframe to display latest data
     df2 = df[['Open','High','Low','Close','avg','l_avg']]
     # Calculate percentage change
-    
-    print(df2.info())
-    
-    # df2[['Change (%)']] = df2['Close'].pct_change() * 100
+    int(df2.info())
+    df2['Change (%)'] = df2['Close'].pct_change() * 100
     # Change index display format
     df2.index = df2.index.strftime('%Y-%m-%d')
     # Rename columns
