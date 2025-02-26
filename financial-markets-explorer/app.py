4bea03a2e3954de2d2d7984719865f97cdcaead1
Mahdi Ghafarian <mahdi.ghaf@outlook.com>
2025-02-25 19:57:28 -0600

    modified:   financial-markets-explorer/app.py

--- financial-markets-explorer/app.py
+++ financial-markets-explorer/app.py
@@ -278,7 +278,7 @@ main_fig.add_shape(
     y0=log_last_close,
     y1=log_last_close,
     line=dict(
-        color="darkorange",
+        color="red",
         width=1,
         dash="dot"
     )
@@ -292,7 +292,7 @@ main_fig.add_annotation(
     showarrow=False,
     yshift=10,
     xshift=50,
-    bgcolor='darkorange',
+    bgcolor='red',
     font=dict(color="white")           
 )
 
