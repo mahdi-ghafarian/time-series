e4478c473bc03a0424ffcf0ab66a84eb0d364a70
Mahdi Ghafarian <mahdi.ghaf@outlook.com>
2025-08-29 21:35:17 -0600

    modified:   financial-markets-explorer/app.py

--- financial-markets-explorer/app.py
+++ financial-markets-explorer/app.py
@@ -145,7 +145,7 @@ st.sidebar.divider()
 st.sidebar.header('Back Transformation',help='Convert the figures from the chart back \
     to their original prices.')
 # get input from user
-log_price = st.sidebar.number_input('Log Price', value=1.0,step=0.1,format="%.2f")
+log_price = st.sidebar.number_input('Log Price', value=1.0,step=0.5,format="%.2f")
 # write the back transformation
 if (log_price):
     st.sidebar.write(f'Price: `{np.exp(log_price):.4f}`')
